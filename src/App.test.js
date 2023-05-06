f64d743a4dc9061a2f626094b040bb7b4ba2bf4d
Sergey Veremiychuk <veremey4uk@gmail.com>
2023-05-06 18:10:24 +0200

    tested count component

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { fireEvent, render, screen } from '@testing-library/react';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import { Counter } from './components/counter'
+
+test('counter increment the count', () => {
+  const {container} = render(<Counter />);
+  const button = container.firstChild;
+  fireEvent.click(button);
+  expect(button.textContent).toBe('1');
 });
--- src/components/counter/index.js
+++ src/components/counter/index.js
@@ -1,7 +1,7 @@
 import {useState} from 'react'
 
 export const Counter = () => {
-  const [count, setCount] = useState(3);
+  const [count, setCount] = useState(0);
   const increment = () => setCount(currentCount => currentCount + 1)
   return <button onClick={increment}>{count}</button>
 }
