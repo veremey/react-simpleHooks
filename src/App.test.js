19cb7288b40fe6665ddba3afb01b09235c24c4c5
Sergey Veremiychuk <veremey4uk@gmail.com>
2023-05-06 18:12:21 +0200

    added default count value [test]

--- src/App.test.js
+++ src/App.test.js
@@ -5,6 +5,8 @@ import { Counter } from './components/counter'
 test('counter increment the count', () => {
   const {container} = render(<Counter />);
   const button = container.firstChild;
+  
+  expect(button.textContent).toBe('0');
   fireEvent.click(button);
   expect(button.textContent).toBe('1');
 });
