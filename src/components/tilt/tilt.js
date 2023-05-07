395a0061f1054ce0d58f1f47563d0e743116b1d1
Sergey Veremiychuk <veremey4uk@gmail.com>
2023-05-07 12:20:14 +0200

    init vanilla-tilt and style it

--- src/components/tilt/tilt.js
+++ src/components/tilt/tilt.js
@@ -1,13 +1,34 @@
-const Tilt = (props) => (
-  <div className="tilt-root">
-    <div className="tilt-child">{props.children}</div>
-  </div>
-)
+import './tilt.css'
+
+import { useEffect, useRef } from 'react'
+
+import VanillaTilt from 'vanilla-tilt'
+
+const Tilt = (props) => {
+  const tiltRef = useRef();
+  
+  useEffect(() => {
+    VanillaTilt.init(tiltRef.current, {
+      max: 25,
+      speed: 400, 
+      glare: true, 
+      'max-glare': 0.5,
+    })
+    return () => tiltRef.current.vanillaTilt.destroy()
+  
+  }, [])
+  
+  return (
+    <div ref={tiltRef} className="tilt-root">
+      <div className="tilt-child">{props.children}</div>
+    </div>
+  )
+}
 
 export const Usage = () => (
   <div className='totally-centered'>
     <Tilt>
-      <div className="totally-centred">Hello tilt</div>
+      <div className="totally-centered">Hello tilt</div>
     </Tilt>
   </div>
 )
